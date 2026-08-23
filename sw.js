0a37f60fcfad5ff80928992ffead906d0fbc7344
lovemilktea05 <m0987414805@gmail.com>
2026-08-23 15:42:17 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'shengyu-manager-v2';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request, { cache: 'no-store' })
+      .catch(() => caches.match(event.request))
+  );
+});
